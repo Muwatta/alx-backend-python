c02a5f9923d9757dcc517716c9971c1baa538f14
Abdullahi Musliudeen Oladipupo <112120730+Muwatta@users.noreply.github.com>
2025-10-05 06:29:33 +0100

    Delete messaging_app directory

--- messaging_app/chats/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsParticipantOfConversation(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Allow only authenticated users
-        if not request.user.is_authenticated:
-            return False
-        # Check if user is a participant in the conversation
-        return obj.participants.filter(id=request.user.id).exists()
