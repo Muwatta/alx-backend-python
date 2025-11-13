d3339f8b83d2e6b52ef142498f0dd3bc27715115
Muwatta <abdullahmusliudeen@gmail.com>
2025-11-13 09:56:53 +0100

    Add custom ORM manager for unread messages

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,20 +1,7 @@
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_page
+from django.shortcuts import render
 from .models import Message
-import django.db.models as models
 
-@login_required
-def delete_user(request):
+def inbox_view(request):
     user = request.user
-    user.delete()
-    return redirect('logout')  # Update if you have a logout view
-
-@cache_page(60)
-@login_required
-def message_list(request, receiver_id):
-    receiver = get_object_or_404(User, id=receiver_id)
-    messages = Message.objects.filter(
-        models.Q(sender=request.user, receiver=receiver) | models.Q(sender=receiver, receiver=request.user)
-    ).select_related('sender', 'receiver').prefetch_related('replies', 'history').order_by('timestamp')
-    return render(request, 'messaging/message_list.html', {'messages': messages, 'receiver': receiver})
+    unread_messages = Message.unread.unread_for_user(user)
+    return render(request, 'messaging/inbox.html', {'messages': unread_messages})
